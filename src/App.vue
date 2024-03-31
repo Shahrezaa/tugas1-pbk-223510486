de17e471c9203cc6fcb4c4ad1127c033b3411db5
Shahreza <Vivoreza344@gmail.com>
2024-03-31 16:00:57 +0700

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <div class="bg">
+    <header>
+      <img
+        alt="Vue logo"
+        class="logo"
+        src="./assets/me1.png"
+        width="180"
+        height="180"
+      />
+
+      <div class="wrapper">
+        <HelloWorld msg="Muhammad Shahreza Khadafi!" />
+      </div>
+    </header>
+  </div>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+.biok {
+  background-color: #294471;
+}
+
+.bg {
+  border-top: 634px solid #294471;
+  border-right: 80px solid hsl(217, 48%, 56%);
+  box-shadow: -10px 10px 10px;
+}
+
+header {
+  line-height: 1.1;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 280px 200px;
+  border-radius: 50%;
+  box-shadow: -10px 10px 10px;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin-top: -600px;
+  }
+
+  header .wrapper {
+    color: #e3d2c8;
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+    margin-top: -450px;
+    margin-left: -500px;
+    margin-right: -100px;
+    font-size: 300px;
+    font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,73 @@
+<script setup>
+import WelcomeItem from "./WelcomeItem.vue";
+import DocumentationIcon from "./icons/IconDocumentation.vue";
+import ToolingIcon from "./icons/IconTooling.vue";
+import EcosystemIcon from "./icons/IconEcosystem.vue";
+import CommunityIcon from "./icons/IconCommunity.vue";
+import SupportIcon from "./icons/IconSupport.vue";
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Ringkasan Profil</template>
+
+    <p>
+      Saya adalah lulusan baru yang berdedikasi untuk membangun karier yang
+      sukses dalam bidang Web Developer. Saya memiliki latar belakang pendidikan
+      yang kuat dalam Prodi Teknik Informatika.
+    </p>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Pendidikan Sekarang</template>
+
+    <p>
+      Fakultas Teknik Prodi Teknik Informatika <br />
+      Universitas Islam Riau, Pekanbaru <br />
+      Angkatan 2022
+    </p>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Keterampilan</template>
+    <p>
+      Keterampilan Bahasa: Bahasa Indonesia (Native). <br />
+      Keterampilan Teknis: Penguasaan Microsoft Office, Bahasa Pemrograman
+      Python, Pengembangan Web dengan HTML/CSS/JavaScript.
+    </p>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Pengalaman Proyek</template>
+
+    <p>
+      Proyek : Pengembangan Aplikasi Manajemen Inventaris menggunakan Python dan
+      MySQL. Bertanggung jawab untuk mengembangkan fitur-fitur utama, dan
+      mengelola basis data.
+    </p>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Kegiatan Ekstrakulikuler</template>
+
+    <p>
+      Anggota aktif dari Klub Pemrograman Alok, mengikuti berbagai kompetisi dan
+      seminar terkait teknologi informasi.
+    </p>
+  </WelcomeItem>
+</template>
